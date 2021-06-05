3825fe8def040cc1bc0b41f64370cfa6f379e71e
EssienAbasiama <essienabasiama11@gmail.com>
2021-06-05 04:14:21 -0700

    Assignment02chapter4

---
+++ Ass03.java
@@ -0,0 +1,29 @@
+public class Ass03 {
+    public static void main(String[] args){
+        System.out.println(Math.sqrt(4));
+        System.out.println(Math.sin(2 * Math.PI));
+        System.out.println(Math.cos(2 * Math.PI));
+        System.out.println(Math.pow(2, 2));
+        System.out.println(Math.log(Math.E));
+        System.out.println(Math.exp(1));
+        
+        System.out.println(Math.rint(-2.5));
+        System.out.println(Math.ceil(-2.5));
+        System.out.println(Math.floor(-2.5));
+        System.out.println(Math.round(-2.5f));
+        System.out.println(Math.rint(2.5));
+        System.out.println(Math.ceil(2.5));
+        System.out.println(Math.floor(2.5));
+
+        System.out.println(Math.round(2.5f));
+        System.out.println(Math.round(2.5));
+        System.out.println(Math.round(Math.abs(-2.5)));
+        System.out.println(Math.E);
+        System.out.println( Math.asin(Math.sin(Math.PI / 6)));
+        System.out.println(Math.PI);
+        System.out.println(Math.sin(Math.asin(Math.PI / 6)));
+    
+
+        
+    }
+}
